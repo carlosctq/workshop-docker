0dfc2b1edadef8e2cba70a5e2e10f0970948b957
Carlos <carlos.cavalcante1@gmail.com>
2025-05-20 14:57:51 -0300

    docker pronto

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def hello_world():
+    return "Olá Turma de Dados! Mudei o codigo Aula de Docker"
+
+def main():
+    st.write(hello_world())
+
+if __name__ == "__main__":
+    main()
+ 
